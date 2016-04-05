56b3ad0823412d2ffd6f824494b05a8938251b25
root <sun__shines@163.com>
2016-04-05 10:42:20 +0800

    monitor host db

---
+++ cloudweb/db/table/static/host.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+class Host:
+    def __init__(self):
+        
+        self.table = 'host'
+        self.id = 'id'
+        self.name = 'name'
+        self.uuid = 'uuid'
+        self.product = 'product'
+        self.manufacturer = 'manufacturer'
+        self.version = 'version'
+        self.serial = 'serial'
+        self.asset_tag = 'asset_tag'
+        self.available = 'available'
+        
+        
+def insert_host(db,name,uuid,product,manuf,ver,serial,tag,avai):
+    
+    h = Host()
+    keys = [h.name,h.uuid,h.product,h.manufacturer,
+            h.version,h.serial,h.asset_tag,h.available]
+    vals = [name,uuid,product,manuf,ver,serial,tag,avai]
+    return db.insert(keys,vals,h.table)
+
+def fetch_host(db,uuid):
+    h = Host()
+    attrs = [h.name,h.uuid,h.product,h.manufacturer,
+            h.version,h.serial,h.asset_tag,h.available]
+    c = {h.uuid:uuid}
+    return db.select(attrs,h.table,c)
+
+def update_host(db,hid,updateattrs):
+    
+    h = Host()
+    d = {}
+    d.update(updateattrs)
+    return db.update(d,h.table,{h.id:hid})
+
+def uuid2attrs(db,uuid):
+    
+    h = Host()
+    attrs = {}
+    datas = fetch_host(db, uuid)
+    if datas:
+        data = datas[0]
+        attrs[h.id] = data[0]
+        attrs[h.name] = data[1]
+        attrs[h.uuid] = data[2]
+        attrs[h.product] = data[3]
+        attrs[h.manufacturer] = data[4]
+        attrs[h.version] = data[5]
+        attrs[h.serial] = data[6]
+        attrs[h.asset_tag] = data[7]
+        attrs[h.available] = data[8]
+        
+    return attrs
+
+def queryattrs(db):
+    
+    h = Host()
+    attrs = {[h.name,h.uuid,h.product,h.manufacturer,
+            h.version,h.serial,h.asset_tag,h.available]}
+    
+    return db.select(attrs,h.table)    
+    
+def puth(db,uuid,attrs):
+    h = Host()
+    dbattrs = uuid2attrs(db, uuid)
+    if not dbattrs:
+        name = attrs.get(h.name,'')
+        uuid = attrs.get(h.uuid)
+        product = attrs.get(h.product,'')
+        manuf = attrs.get(h.manufacturer,'')
+        ver = attrs.get(h.version,'')
+        serial = attrs.get(h.serial,'')
+        tag = attrs.get(h.asset_tag,'')
+        avai = 'connected'
+        return insert_host(db, name, uuid, product, manuf, ver, serial, tag, avai)
+    
+    d = {}
+    for key in attrs:
+        if dbattrs[key] != attrs.get(key,''):
+            d[key] = attrs.get(key,'')
+    if d:
+        return update_host(db, dbattrs[h.id],d)
+    return True,''
---
+++ cloudweb/db/table/static/host_cpu.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+class Cpu:
+    def __init__(self):
+        self.table = 'host_cpu'
+        self.hid = 'host_id'
+        self.id = 'id'
+        self.desc = 'descrition'
+        self.product = 'product'
+        self.vendor = 'vendor'
+        self.phyid = 'physical_id'
+        self.bus = 'bus_info'
+        self.version = 'version'
+        self.size = 'size'
+        self.capacity = 'capacity'
+        self.width = 'width'
+        self.clock = 'clock'
+        self.cores = 'cores'
+        self.enabledcores = 'enabledcores'
+        self.threads = 'threads'
+        
+def insert_cpu(db,hid,desc,product,vendor,phyid,bus,version,
+               size,capacity,width,clock,cores,enabledcores,threads):
+    
+    c = Cpu()
+    keys = [c.hid,c.desc,c.product,c.vendor,c.phyid,c.bus,c.version,
+            c.size,c.capacity,c.width,c.clock,c.cores,c.enabledcores,c.threads]
+    vals = [hid,desc,product,vendor,phyid,bus,version,
+            size,capacity,width,clock,cores,enabledcores,threads]
+    return db.insert(keys,vals,c.table)
+
+def fetch_cpu(db,hid):
+    c = Cpu()
+    attrs = [c.hid,c.id,c.desc,c.product,c.vendor,c.phyid,c.bus,c.version,
+             c.size,c.capacity,c.width,c.clock,c.cores,c.enabledcores,c.threads]
+    cond = {c.hid:hid}
+    return db.select(attrs,c.table,cond)
+
+def update_cpu(db,hid,updateattrs):
+    c = Cpu()
+    d = {}
+    d.update(updateattrs)
+    return db.update(d,c.table,{c.hid:hid})
+
+def hid2attrs(db,hid):
+    c = Cpu()
+    attrs = {}
+    datas = fetch_cpu(db, hid)
+    if datas:
+        data = datas[0]
+        attrs[c.id] = data[0]
+        attrs[c.hid] = data[1]
+        attrs[c.desc] = data[2]
+        attrs[c.product] = data[3]
+        attrs[c.vendor] = data[4]
+        attrs[c.phyid] = data[5]
+        attrs[c.bus] = data[6]
+        attrs[c.version] = data[7]
+        attrs[c.size] = data[8]
+        attrs[c.capacity] = data[9]
+        attrs[c.width] = data[10]
+        attrs[c.clock] = data[11]
+        attrs[c.cores] = data[12]
+        attrs[c.enabledcores] = data[13]
+        attrs[c.threads] = data[14]
+        
+    return attrs
+
+def queryattrs(db):
+    pass
+
+def putc(db,hid,attrs):
+    c = Cpu()
+    dbattrs = hid2attrs(db, hid)
+    if not dbattrs:
+        host_id = hid
+        desc = attrs.get(c.desc,'')
+        product = attrs.get(c.product,'')
+        vendor = attrs.get(c.vendor,'')
+        phyid = attrs.get(c.phyid,'')
+        bus = attrs.get(c.bus,'')
+        ver = attrs.get(c.version,'')
+        size = attrs.get(c.size,'')
+        capacity = attrs.get(c.capacity,'')
+        width = attrs.get(c.width,'')
+        clock = attrs.get(c.clock,'')
+        cores = attrs.get(c.cores,'')
+        enabledcores = attrs.get(c.enabledcores,'')
+        threads = attrs.get(c.threads,'')
+        
+        return insert_cpu(db, host_id,desc,product,vendor,phyid,bus,ver,
+                          size,capacity,width,clock,cores,enabledcores,threads)
+    
+    d = {}
+    for key in attrs:
+        if dbattrs[key] != attrs.get(key,''):
+            d[key] = attrs.get(key,'')
+    if d:
+        return update_cpu(db, dbattrs[c.hid],d)
+    return True,''
