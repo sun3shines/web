7d4ad2859467659d82397190ea4834fcf1901b66
root <sun__shines@163.com>
2016-04-25 10:58:44 +0800

    libproxy and storage

--- cloudexecutor/libconfig/libproxy.py
+++ cloudexecutor/libconfig/libproxy.py
@@ -1,1 +1,31 @@
 # -*- coding: utf-8 -*-
+
+from cloudexecutor.libconfig.libst import _stget,_stset
+from cloudlib.globalx.config import EX_ACCOUNT_CONF_PATH,EX_CONTAINER_CONF_PATH,\
+    EX_OBJECT_CONF_PATH,EX_PROXY_CONF_PATH
+
+conf2st = {'proxy_bind_port':[(EX_PROXY_CONF_PATH,'DEFAULT','bind_port')]}
+
+def libSetProxy(attr):
+    for key,val in attr.items():
+        if key not in conf2st:
+            continue
+        for path,sec,option in conf2st.get(key):
+            _stset(path, sec, option, val)
+             
+def libGetProxy():
+    
+    attr = {}
+    for key in conf2st:
+        val = ''
+        for path,sec,option in conf2st.get(key):
+            val = _stget(path, sec, option)
+            attr.update({key:val})
+            break
+    return attr
+
+if __name__ == '__main__':
+    print libGetProxy()
+    libSetProxy({'proxy_bind_port':'556'})
+    print libGetProxy()
+    
--- cloudexecutor/libconfig/libstorage.py
+++ cloudexecutor/libconfig/libstorage.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+
+from cloudexecutor.libconfig.libst import _stget,_stset
+from cloudlib.globalx.config import EX_ACCOUNT_CONF_PATH,EX_CONTAINER_CONF_PATH,\
+    EX_OBJECT_CONF_PATH,EX_PROXY_CONF_PATH
+    
+conf2st = {'storage_devices':[(EX_PROXY_CONF_PATH,'DEFAULT','devices'),(EX_ACCOUNT_CONF_PATH,'DEFAULT','devices'),
+                              (EX_CONTAINER_CONF_PATH,'DEFAULT','devices'),(EX_OBJECT_CONF_PATH,'DEFAULT','devices')]}
+
+def libSetStorage(attr):
+    for key,val in attr.items():
+        if key not in conf2st:
+            continue
+        for path,sec,option in conf2st.get(key):
+            _stset(path, sec, option, val)
+             
+def libGetStorage():
+    
+    attr = {}
+    for key in conf2st:
+        val = ''
+        for path,sec,option in conf2st.get(key):
+            val = _stget(path, sec, option)
+            attr.update({key:val})
+            break
+    return attr
+
+if __name__ == '__main__':
+#    print libGetStorage()
+#    libSetStorage({'storage_devices':'/srv'})
+#    print libGetStorage()
+
+    libSetStorage({'storage_devices':'/mnt/cloudfs-object'})
+    print libGetStorage()
