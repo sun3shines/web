edfeeed1f50ef75a9e572df0394fed91c4da0cfc
root <sun__shines@163.com>
2016-04-25 10:49:35 +0800

    libcache and libconcurrency

--- cloudexecutor/libconfig/libcache.py
+++ cloudexecutor/libconfig/libcache.py
@@ -1,1 +1,48 @@
 # -*- coding: utf-8 -*-
+
+from cloudexecutor.libconfig.libst import _stget,_stset
+from cloudlib.globalx.config import EX_ACCOUNT_CONF_PATH,EX_CONTAINER_CONF_PATH,\
+    EX_OBJECT_CONF_PATH,EX_PROXY_CONF_PATH
+    
+conf2st = {'cache_memcache_host':[(EX_PROXY_CONF_PATH,'filter:cache','memcache_servers')],
+           'cache_memcache_port':[(EX_PROXY_CONF_PATH,'filter:cache','memcache_servers')]}
+
+def libSetCache(attr):
+    
+    for key,val in attr.items():
+        if key not in conf2st:
+            continue
+        for path,sec,option in conf2st.get(key):
+            old_vals = _stget(path, sec, option)
+            old_host = old_vals.split(':')[0]
+            old_port = old_vals.split(':')[1]
+            if 'cache_memcache_host' == key:
+                new_vals = ':'.join([val,old_port])
+            else:
+                new_vals = ':'.join([old_host,val])
+            _stset(path, sec, option, new_vals)
+             
+def libGetCache():
+    
+    attr = {}
+    for key in conf2st:
+        val = ''
+        for path,sec,option in conf2st.get(key):
+            old_vals = _stget(path, sec, option)
+            
+            if 'cache_memcache_host' == key:
+                val = old_vals.split(':')[0] 
+            else:
+                val = old_vals.split(':')[1] 
+            attr.update({key:val})
+            break
+    return attr
+
+if __name__ == '__main__':
+    # print libGetCache()
+#    libSetCache({'cache_memcache_host':'1.1.1.1'})
+#    libSetCache({'cache_memcache_port':'11234'})
+    libSetCache({'cache_memcache_host':'127.0.0.1',
+                'cache_memcache_port':'11211'})
+    print libGetCache()
+
--- cloudexecutor/libconfig/libconcurrency.py
+++ cloudexecutor/libconfig/libconcurrency.py
@@ -1,1 +1,43 @@
 # -*- coding: utf-8 -*-
+
+from cloudexecutor.libconfig.libst import _stget,_stset
+from cloudlib.globalx.config import EX_ACCOUNT_CONF_PATH,EX_CONTAINER_CONF_PATH,\
+    EX_OBJECT_CONF_PATH,EX_PROXY_CONF_PATH
+    
+conf2st = {'concurrency_proxy':[(EX_PROXY_CONF_PATH,'DEFAULT','workers')],
+           'concurrency_account':[(EX_ACCOUNT_CONF_PATH,'DEFAULT','workers')],
+           'concurrency_container':[(EX_CONTAINER_CONF_PATH,'DEFAULT','workers')],
+           'concurrency_object':[(EX_OBJECT_CONF_PATH,'DEFAULT','workers')]}
+
+def libSetConCurrency(attr):
+    for key,val in attr.items():
+        if key not in conf2st:
+            continue
+        for path,sec,option in conf2st.get(key):
+            _stset(path, sec, option, val)
+             
+def libGetConCurrency():
+    
+    attr = {}
+    for key in conf2st:
+        val = ''
+        for path,sec,option in conf2st.get(key):
+            val = _stget(path, sec, option)
+            attr.update({key:val})
+            break
+    return attr
+
+if __name__ == '__main__':
+    
+    #print libGetConCurrency()
+    #libSetConCurrency({'concurrency_proxy':'2',
+    #                   'concurrency_account':'3',
+    #                   'concurrency_container':'4',
+    #                   'concurrency_object':'5'})
+    #print libGetConCurrency()
+    libSetConCurrency({'concurrency_proxy':'0',
+                       'concurrency_account':'0',
+                       'concurrency_container':'0',
+                       'concurrency_object':'0'})
+    print libGetConCurrency()
+    
